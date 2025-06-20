5eace48eb084e7ee66f4f2567b3f59645b2ba27a
Hari <amarhari26122002@gmail.com>
2025-06-20 22:06:31 +0530

    duplication error fixed

--- src/components/TextToImage.jsx
+++ src/components/TextToImage.jsx
@@ -56,7 +56,7 @@ const TextToImage = () => {
           background-size: 300% 300%;
         }
       `}</style>
-<<<<<<< HEAD
+
 <div className="p-4 max-w-md w-full mx-auto mt-10">
   <div className="w-full aspect-[1/1] flex items-center justify-center rounded overflow-hidden">
     {loading ? (
@@ -105,50 +105,8 @@ const TextToImage = () => {
   </div>
 </div>
 
-=======
 
-      <div className="p-4 max-w-md mx-auto mt-10">
-        <div className=" w-[400px] h-[400px] flex items-center justify-center rounded overflow-hidden">
-          {loading ? (
-            <div className="w-full h-full rounded animate-gradientMove bg-gradient-to-r from-pink-300 via-fuchsia-300 to-rose-300 "  />
-          ) : (
-            imageUrl && (
-              <div className="mt-4 border-2 border-white">
-                <img src={imageUrl} alt="Generated" className="rounded shadow-lg mb-2 " />
-              </div>
-            )
-          )}
-        </div>
 
-        <textarea
-          className="w-full p-2 border rounded mb-2 text-white bg-black focus:outline-none mt-3"
-          rows="4"
-          placeholder="Enter your prompt..."
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-        />
-
-        <div className="flex gap-2 mb-4">
-          <button
-            className="bg-pink-500 text-white px-4 py-2 rounded"
-            onClick={handleGenerate}
-            disabled={loading}
-          >
-            {loading ? 'Generating...' : 'Generate Image'}
-          </button>
-
-          <button
-            className={`px-4 py-2 rounded ${
-              imageUrl ? 'bg-green-500 text-white' : 'bg-gray-300 text-gray-500 cursor-not-allowed'
-            }`}
-            onClick={handleDownload}
-            disabled={loading}
-          >
-            Download Image
-          </button>
-        </div>
-      </div>
->>>>>>> 35f20bc7ec5ca7ffd8d951db9eb1ea59bb8023eb
     </>
   );
 };
